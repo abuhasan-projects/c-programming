3d2ab9470d186ac2ab427050008baa2437b9f61e
dev-abuhasan <ahinstitutte@gmail.com>
2022-04-26 00:35:16 +0600

    add data type

--- c_03_data-type.c
+++ c_03_data-type.c
@@ -3,5 +3,16 @@
 int main(){
     printf("C Data Type\n");
 
+    //Basic Data Type
+    /****
+     * int	    2 or 4 bytes	Stores whole numbers, without decimals
+     * float	4 bytes	Stores  fractional numbers, containing one or more decimals. Sufficient for storing 7 decimal digits
+     * double	8 bytes	Stores  fractional numbers, containing one or more decimals. Sufficient for storing 15 decimal digits
+     * char	    1 byte	Stores  a single character/letter/number, or ASCII values
+     ****/
+
+    double myDoubleNum = 19.99;  // Double (floating point number)
+    printf("%lf\n", myDoubleNum);
+
     return 0;
 }
