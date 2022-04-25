d46bdbcfbec86c7712296bfb8e61b301847d0587
dev-abuhasan <ahinstitutte@gmail.com>
2022-04-26 00:23:25 +0600

    add data type

--- c_02_var.c
+++ c_02_var.c
@@ -3,17 +3,31 @@
 int main() {
     int roll = 120;
     printf("C Variable \n");
-    printf("%d", roll);
+    printf("%d\n", roll);
 
     int myNum = 5;             // Integer (whole number)
     float myFloatNum = 5.99;   // Floating point number
     char myLetter = 'D';       // Character
-    // Print variables
-    printf("%d\n", myNum);
-    printf("%f\n", myFloatNum);
-    printf("%c\n", myLetter);
 
-    printf("My number is %d and my letter is %c", myNum, myLetter);
+    // Print variables
+    printf("My number is %d, letter is %c & float is %f", myNum, myLetter, myFloatNum);
+
+    //    sum
+    int sum_a = 5;
+    int sum_b = 6;
+    float sum_c = 5.3;
+
+    int sum = sum_a + sum_b;
+    printf("\nSum Tow Number %d\n", sum);
+    printf("Sum with float num %f\n", sum_a + sum_b + sum_c);
+
+    int x = 5, y = 6, z = 50;
+    printf("Multiple Value %d\n", x + y + z);
+
+    int x1, y1, z1;
+    x1 = y1 = z1 = 50;
+    printf("Same value %d\n", x1 + y1 + z1);
+
 
     return 0;
 }
---
+++ c_03_data-type.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+    printf("C Data Type\n");
+
+    return 0;
+}
