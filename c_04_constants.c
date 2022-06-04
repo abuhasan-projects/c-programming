13408f2a496af47be7cab96609f1652e2d7fb871
dev-abuhasan <ahinstitutte@gmail.com>
2022-06-04 12:30:50 +0600

    add constant

--- c_04_constants.c
+++ c_04_constants.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
-int main(){
+int main() {
     char num0 = '0';
     int numFile = 4;
-    printf("C Constants - %c%d \n", num0,numFile);
+    printf("C Constants - %c%d \n", num0, numFile);
 
+    //    const var
+    const int firstConstantNum = 726;
+    printf("Hello Constant Number %d \n", firstConstantNum);
 
     return 0;
 }
