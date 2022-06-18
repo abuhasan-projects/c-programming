257da4a7244add990a0b598680ccd183140b2d97
dev-abuhasan <ahinstitutte@gmail.com>
2022-06-18 11:41:25 +0600

    01 Issue with scan f

---
+++ DS/c_01_time_complexity.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main () {
+    printf("C Time Complexity");
+
+    return 0;
+}
--- c_09_function.c
+++ c_09_function.c
@@ -11,6 +11,12 @@ int returnValFun(int x, int y) {
 }
 int sum(int k);
 
+struct Car {
+    char brand[50];
+    char model[50];
+    int year;
+};
+
 
 int main (){
     printf("C Function \n");
@@ -34,6 +40,16 @@ int main (){
      * tan(x)
      * ***/
     printf("sqrt: %f ", sqrt(16));
+
+
+
+    struct Car car1 = {"BMW", "X5", 1999};
+    struct Car car2 = {"Ford", "Mustang", 1969};
+    struct Car car3 = {"Toyota", "Corolla", 2011};
+
+    printf("%s %s %d\n", car1.brand, car1.model, car1.year);
+    printf("%s %s %d\n", car2.brand, car2.model, car2.year);
+    printf("%s %s %d\n", car3.brand, car3.model, car3.year);
 
     return 0;
 }
