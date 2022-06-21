fcae866232b34f87142ce0ce53149913bf5c1d41
dev-abuhasan <ahinstitutte@gmail.com>
2022-06-21 13:51:39 +0600

    01 Issue with scan f

--- DS/c_01_time_complexity.c
+++ DS/c_01_time_complexity.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
-int main () {
+
+int main() {
     printf("C Time Complexity");
+
+    int i, j, n, count;
+    scanf("%d", &n);
+
+    count = 0;
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            // count = count + 1;
+            count++;
+            // printf("%d\n", j);
+        }
+    }
+    printf("n = %d, count = %d \n", n, count);
 
     return 0;
 }
